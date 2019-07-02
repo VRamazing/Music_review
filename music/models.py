f9ac13448bd844de10bcd8a88fc523b7e2c92c94
vignesh ramesh <r.vignesh149@gmail.com>
2019-07-02 17:36:17 +0530

    Linester complete with auhthentication, authorization, album, song creation

--- music/models.py
+++ music/models.py
@@ -1,19 +1,23 @@
+from django.contrib.auth.models import Permission, User
 from django.db import models
 
-# Create your models here.
+
 class Album(models.Model):
+    user = models.ForeignKey(User, default=1)
     artist = models.CharField(max_length=250)
-    album_title = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
 
+
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
